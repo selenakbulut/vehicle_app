bb5c7bf5be9687bf2344de07e54e26af5ef24365
Selen Akbulut <akbulutselennur@gmail.com>
2023-09-06 10:58:54 +0300

    server client updated

--- haberlesme/haberlesme_client.py
+++ haberlesme/haberlesme_client.py
@@ -10,12 +10,20 @@ def run():
     print("Will try to greet world ...")
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = com_pb2_grpc.HaberlesmeStub(channel)
-       
-        car = stub.showCar(com_pb2.NameRequest(name="engin"))
+      
+        car = stub.showCar(com_pb2.NameRequest(name= "selen"))
         print("Kisinin arabasi: " + car.brand)
         print("Arabanin plakasi: " + car.plate)
         print("Arac kilometresi: " + str(car.kilometer))
         print("Arac sahibi: " + car.owner)
+
+        person_reply = stub.getCars(com_pb2.NameRequest(name= "engin"))
+        car = person_reply.cars
+        for car in person_reply.cars:
+            print("Kisinin arabasi: " + car.brand)
+            print("Arabanin plakasi: " + car.plate)
+            print("Arac kilometresi: " + str(car.kilometer))
+            print("Arac sahibi: " + car.owner)
  
 if __name__ == "__main__":
     logging.basicConfig()
