a85c77e62d8a614cdfdc4d008a53a82ad5801bd9
Selen Akbulut <akbulutselennur@gmail.com>
2023-09-12 14:33:02 +0300

    communication file added

---
+++ communication/__init__.py
@@ -0,0 +1,2 @@
+#"Haberlesme" alt dizininin bir Python paketi olarak tanınması için alt dizini içinde bir __init__.py dosyası bulunması gerekiyor.
+#  Bu dosya, alt dizini bir paket olarak işaretler.
