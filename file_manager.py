6d05383416e521419fc97e83969eb3ab4ec37232
Selen Akbulut <akbulutselennur@gmail.com>
2023-09-12 14:17:40 +0300

    translated english

--- file_manager.py
+++ file_manager.py
@@ -3,96 +3,94 @@ import json
 
 def check_file(file):
     if not os.path.exists(file):
-            data = {"kisiler": []}
+            data = {"persons": []}
             with open(file, 'w', encoding='utf-8') as file:
                 json.dump(data, file, indent =4)
 
 class Manager:
     def __init__(self) -> None:
-         self.kisiler = None
+         self.persons = None
          self.brand = None
          self.plate = None
          self.kilometer = None
          self.veri = str
-         self.dosya_yolu = 'kisiler.json'
+         self.dosya_yolu = 'persons.json'
          check_file(self.dosya_yolu)
          with open(self.dosya_yolu, 'r', encoding='utf-8') as file:
             self.veri = json.load(file)
-            self.kisiler = self.veri.get('kisiler', [])
+            self.persons = self.veri.get('persons', [])
       
     def get_cars(self, person_name):
         vehicles = []
 
         with open(self.dosya_yolu, 'r', encoding='utf-8') as file:
             self.veri = json.load(file)
-            self.kisiler = self.veri.get('kisiler', [])
-            self.brand = self.veri.get('arabalar', [])
+            self.persons = self.veri.get('persons', [])
+            self.brand = self.veri.get('cars', [])
         
-        for kisi in self.kisiler:
-            if person_name == kisi.get('kisi'):
-                arabalar = kisi.get('arabalar', [])
-                for araba in arabalar:
-                    brand = araba.get('araba')
-                    plate = araba.get('plaka')
-                    kilometer = araba.get('kilometre')
-                    owner = kisi.get('kisi')
+        for person in self.persons:
+            if person_name == person.get('person'):
+                cars = person.get('cars', [])
+                for car in cars:
+                    brand = car.get('car')
+                    plate = car.get('plate')
+                    kilometer = car.get('kilometer')
+                    owner = person.get('person')
 
                     car_info = {
-                        "Arac markasi": brand,
-                        "Arac plakasi": plate,
-                        "Arac kilometresi": kilometer,
-                        "Arac sahibi": owner 
+                        "Car brand": brand,
+                        "Car plate": plate,
+                        "Car kilometer": kilometer,
+                        "Car owner": owner 
                     }
                     vehicles.append(car_info)        
         return vehicles
     
     def addCartoPerson(self, person_name, brand, plate, kilometer):
         new_car_info = {
-            "araba": brand,
-            "kilometre": kilometer,
-            "plaka": plate,
+            "car": brand,
+            "kilometer": kilometer,
+            "plate": plate,
         }
 
-        for kisi in self.kisiler:
-            if person_name == kisi.get('kisi'):
-                print(kisi.get('kisi'))
-                arabalar = kisi.get('arabalar', [])
-                print(arabalar)
-                for araba in arabalar:
-                    print(araba.get('plaka'))
-                    if plate == araba.get('plaka'):
+        for person in self.persons:
+            if person_name == person.get('person'):
+                cars = person.get('cars', [])
+                
+                for car in cars:
+                    if plate == car.get('plate'):
                         print(f"Plate number {plate} already exists for {person_name}")
                         break
                 else:
-                    kisi['arabalar'].append(new_car_info)
+                    person['cars'].append(new_car_info)
                 break   
                 
         else:
             new_person = {
-            "kisi": person_name,
-            "arabalar": [new_car_info]
+            "person": person_name,
+            "cars": [new_car_info]
         }
-            self.kisiler.append(new_person)
+            self.persons.append(new_person)
             
-        self.veri['kisiler'] = self.kisiler
+        self.veri['persons'] = self.persons
         with open(self.dosya_yolu, 'w', encoding='utf-8') as file:
             json.dump(self.veri, file, indent=4)
 
     def km_update(self, person_name, plate, new_km):
-        for kisi in self.kisiler:
-            if person_name == kisi.get('kisi'):
-                arabalar = kisi.get('arabalar', [])
+        for person in self.persons:
+            if person_name == person.get('person'):
+                cars = person.get('cars', [])
 
-                for araba in arabalar:
-                    if plate == araba.get('plaka'):
-                        old_km = araba.get('kilometre')
+                for car in cars:
+                    if plate == car.get('plate'):
+                        old_km = car.get('kilometer')
                         if new_km >= old_km:
-                            araba['kilometre'] = new_km
+                            car['kilometer'] = new_km
                         else:
                             print("New kilometer cannot be less than older kilometer.")
                         break
                 break
 
-        self.veri['kisiler'] = self.kisiler
+        self.veri['persons'] = self.persons
         with open(self.dosya_yolu, 'w', encoding='utf-8') as file:
             json.dump(self.veri, file, indent=4)    
